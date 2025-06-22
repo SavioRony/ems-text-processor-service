38b2b2f16cc4817059718e23955c7e5278af222e
savio <savio_ls@hotmail.com>
2025-06-22 06:52:09 -0300

    Add

---
+++ src/main/java/com/arq/text_processor/TextProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.arq.text_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arq/text_processor/TextProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arq.text_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
